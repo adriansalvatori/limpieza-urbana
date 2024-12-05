42b9ade2de6505cf5259fd44321841b6bc371278
Adrián Salvatori <adriansalvatori@gmail.com>
2024-12-05 15:38:16 -0500

    Styling and structuring routing

--- app/routes/pqr.tsx
+++ app/routes/pqr.tsx
@@ -8,7 +8,6 @@ const PQR = () => {
                     <div className="container is-max-desktop">
                         <div className="container is-max-desktop has-text-centered pt-6">
                             <h1 className="title is-1"><span className="has-text-weight-bold">PQR</span> (Peticiones, Quejas y Recursos)</h1>
-                            <h2 className="subtitle is-4">Intervención técnica de poda de árboles</h2>
                         </div>
                     </div>
                 </div>
